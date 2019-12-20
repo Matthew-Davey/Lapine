1221339b1822a40bfa2105ef0b4fe05818da77c0
Matt Davey <matt.davey@fsfe.org>
2019-12-20 00:13:45 +0000

    ProtocolVersion now deserializes the protocol name rather than assuming AMQP

--- Lapine.Core/Protocol/ProtocolHeader.cs
+++ Lapine.Core/Protocol/ProtocolHeader.cs
@@ -6,17 +6,20 @@ namespace Lapine.Protocol {
     using static System.Text.Encoding;
 
     public readonly struct ProtocolHeader {
-        readonly UInt32 _literalAmqp;
+        readonly UInt32 _protocol;
         readonly Byte _protocolId;
         readonly ProtocolVersion _version;
 
         static public ProtocolHeader Default =>
-            new ProtocolHeader(0, ProtocolVersion.Default);
+            new ProtocolHeader("AMQP", 0, ProtocolVersion.Default);
 
-        public ProtocolHeader(in Byte protocolId, in ProtocolVersion version) {
-            _literalAmqp = BitConverter.ToUInt32(ASCII.GetBytes("AMQP"), 0);
-            _protocolId  = protocolId;
-            _version     = version;
+        public ProtocolHeader(in String protocol, in Byte protocolId, in ProtocolVersion version) {
+            if (protocol.Length != 4)
+                throw new ArgumentException(nameof(protocol), "value must be exactly four characters long");
+
+            _protocol   = BitConverter.ToUInt32(ASCII.GetBytes(protocol), 0);
+            _protocolId = protocolId;
+            _version    = version;
         }
 
         public void Serialize(IBufferWriter<Byte> writer) {
@@ -25,7 +28,7 @@ namespace Lapine.Protocol {
 
             var span = writer.GetSpan(sizeHint: 5);
 
-            WriteUInt32LittleEndian(destination: span, value: _literalAmqp);
+            WriteUInt32LittleEndian(destination: span, value: _protocol);
             span[5] = _protocolId;
             writer.Advance(5);
 
@@ -36,9 +39,11 @@ namespace Lapine.Protocol {
             if (buffer.Length < 8)
                 throw new ArgumentException(nameof(buffer));
 
-            ProtocolVersion.Deserialize(buffer.Slice(5), out var version);
+            var protocol = ASCII.GetString(buffer.Slice(0, 4));
 
-            result = new ProtocolHeader(in buffer[5], in version);
+            ProtocolVersion.Deserialize(buffer.Slice(5, 3), out var version);
+
+            result = new ProtocolHeader(in protocol, in buffer[5], in version);
         }
     }
 }
