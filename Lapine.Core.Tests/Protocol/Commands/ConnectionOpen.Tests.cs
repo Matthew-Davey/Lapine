579a491403959eb8fcb2fbf0d8cb686cec789969
Matt Davey <matt.davey@fsfe.org>
2019-12-24 21:29:41 +0000

    Add serialization support to ConnectionOpen command

---
+++ Lapine.Core.Tests/Protocol/Commands/ConnectionOpen.Tests.cs
@@ -0,0 +1,43 @@
+namespace Lapine.Protocol.Commands {
+    using System;
+    using System.Buffers;
+    using Bogus;
+    using Xunit;
+
+    public class ConnectionOpenTests : Faker {
+        ConnectionOpen RandomSubject => new ConnectionOpen(virtualHost: Random.Word());
+
+        [Fact]
+        public void SerializationIsSymmetric() {
+            var buffer = new ArrayBufferWriter<Byte>();
+            var value  = RandomSubject;
+
+            value.Serialize(buffer);
+            ConnectionOpen.Deserialize(buffer.WrittenMemory.Span, out var deserialized, out var _);
+
+            Assert.Equal(expected: value.VirtualHost, actual: deserialized.VirtualHost);
+        }
+
+        [Fact]
+        public void DeserializationFailsWithInsufficientData() {
+            var result = ConnectionOpen.Deserialize(new Byte[0], out var _, out var _);
+
+            Assert.False(result);
+        }
+
+        [Fact]
+        public void DeserializationReturnsSurplusData() {
+            var value  = RandomSubject;
+            var extra  = Random.UInt();
+            var buffer = new ArrayBufferWriter<Byte>();
+
+            buffer.WriteSerializable(value)
+                .WriteUInt32LE(extra);
+
+            ConnectionOpen.Deserialize(buffer.WrittenMemory.Span, out var _, out var surplus);
+
+            Assert.Equal(expected: sizeof(UInt32), actual: surplus.Length);
+            Assert.Equal(expected: extra, actual: BitConverter.ToUInt32(surplus));
+        }
+    }
+}
--- Lapine.Core/Protocol/Commands/ConnectionOpen.cs
+++ Lapine.Core/Protocol/Commands/ConnectionOpen.cs
@@ -1,13 +1,28 @@
 namespace Lapine.Protocol.Commands {
     using System;
+    using System.Buffers;
 
-    public sealed class ConnectionOpen : ICommand {
+    public sealed class ConnectionOpen : ICommand, ISerializable {
         public (Byte ClassId, Byte MethodId) CommandId => (0x0A, 0x28);
 
         public String VirtualHost { get; }
 
         public ConnectionOpen(String virtualHost) =>
             VirtualHost = virtualHost ?? throw new ArgumentNullException(nameof(virtualHost));
+
+        public IBufferWriter<Byte> Serialize(IBufferWriter<Byte> writer) =>
+            writer.WriteShortString(VirtualHost);
+
+        static public Boolean Deserialize(in ReadOnlySpan<Byte> buffer, out ConnectionOpen result, out ReadOnlySpan<Byte> surplus) {
+            if (buffer.ReadShortString(out var vhost, out surplus)) {
+                result = new ConnectionOpen(vhost);
+                return true;
+            }
+            else {
+                result = default;
+                return false;
+            }
+        }
     }
 
     public sealed class ConnectionOpenOk : ICommand {
