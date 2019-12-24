4e00ec4f71ea429d0aff2c20b17252d4e8a70798
Matt Davey <matt.davey@fsfe.org>
2019-12-24 21:55:58 +0000

    Add serialization support to ConnectionClose command

--- Lapine.Core/Protocol/Commands/ConnectionClose.cs
+++ Lapine.Core/Protocol/Commands/ConnectionClose.cs
@@ -1,19 +1,39 @@
 namespace Lapine.Protocol.Commands {
     using System;
+    using System.Buffers;
 
-    public sealed class ConnectionClose : ICommand {
+    public sealed class ConnectionClose : ICommand, ISerializable {
         public (Byte ClassId, Byte MethodId) CommandId => (0x0A, 0x32);
 
         public UInt16 ReplyCode { get; }
         public String ReplyText { get; }
-        public (Byte ClassId, Byte MethodId) FailingMethod { get; }
-        public Byte FailingClassId { get; }
-        public Byte FailingMethodId { get; }
+        public (UInt16 ClassId, UInt16 MethodId) FailingMethod { get; }
 
-        public ConnectionClose(UInt16 replyCode, String replyText, (Byte ClassId, Byte MethodId) failingMethod) {
+        public ConnectionClose(UInt16 replyCode, String replyText, (UInt16 ClassId, UInt16 MethodId) failingMethod) {
             ReplyCode     = replyCode;
             ReplyText     = replyText ?? throw new ArgumentNullException(nameof(replyText));
             FailingMethod = failingMethod;
+        }
+
+        public IBufferWriter<Byte> Serialize(IBufferWriter<Byte> writer) =>
+            writer.WriteUInt16BE(ReplyCode)
+                .WriteShortString(ReplyText)
+                .WriteUInt16BE(FailingMethod.ClassId)
+                .WriteUInt16BE(FailingMethod.MethodId);
+
+        static public Boolean Deserialize(ReadOnlySpan<Byte> buffer, out ConnectionClose result, out ReadOnlySpan<Byte> surplus) {
+            if (buffer.ReadUInt16BE(out var replyCode, out surplus) &&
+                surplus.ReadShortString(out var replyText, out surplus) &&
+                surplus.ReadUInt16BE(out var failingClassId, out surplus) &&
+                surplus.ReadUInt16BE(out var failingMethodId, out surplus))
+            {
+                result = new ConnectionClose(replyCode, replyText, (failingClassId, failingMethodId));
+                return true;
+            }
+            else {
+                result = default;
+                return false;
+            }
         }
     }
 
