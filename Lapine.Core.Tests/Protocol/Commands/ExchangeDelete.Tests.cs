c3ad2c91857822ee5cb9029428a1428156712c7a
Matt Davey <matt.davey@fsfe.org>
2019-12-27 02:06:06 +0000

    Add serialization support to ExchangeDelete and ExchangeDeleteOk commands

---
+++ Lapine.Core.Tests/Protocol/Commands/ExchangeDelete.Tests.cs
@@ -0,0 +1,66 @@
+namespace Lapine.Protocol.Commands {
+    using System;
+    using System.Buffers;
+    using Bogus;
+    using Xunit;
+
+    public class ExchangeDeleteTests : Faker {
+        ExchangeDelete RandomSubject => new ExchangeDelete(
+            exchangeName: Random.Word(),
+            ifUnused    : Random.Bool(),
+            noWait      : Random.Bool()
+        );
+
+        [Fact]
+        public void SerializationIsSymmetric() {
+            var buffer = new ArrayBufferWriter<Byte>(8);
+            var value  = RandomSubject;
+
+            value.Serialize(buffer);
+            ExchangeDelete.Deserialize(buffer.WrittenMemory.Span, out var deserialized, out var _);
+
+            Assert.Equal(expected: value.ExchangeName, actual: deserialized.ExchangeName);
+            Assert.Equal(expected: value.IfUnused, actual: deserialized.IfUnused);
+            Assert.Equal(expected: value.NoWait, actual: deserialized.NoWait);
+        }
+
+        [Fact]
+        public void DeserializationFailsWithInsufficientData() {
+            var result = ExchangeDelete.Deserialize(new Byte[0], out var _, out var _);
+
+            Assert.False(result);
+        }
+
+        [Fact]
+        public void DeserializationReturnsSurplusData() {
+            var value  = RandomSubject;
+            var extra  = Random.UInt();
+            var buffer = new ArrayBufferWriter<Byte>();
+
+            buffer.WriteSerializable(value)
+                .WriteUInt32LE(extra);
+
+            ExchangeDelete.Deserialize(buffer.WrittenMemory.Span, out var _, out var surplus);
+
+            Assert.Equal(expected: sizeof(UInt32), actual: surplus.Length);
+            Assert.Equal(expected: extra, actual: BitConverter.ToUInt32(surplus));
+        }
+    }
+
+    public class ExchangeDeleteOkTests : Faker {
+        [Fact]
+        public void DeserializationReturnsSurplusData() {
+            var value  = new ExchangeDeleteOk();
+            var extra  = Random.UInt();
+            var buffer = new ArrayBufferWriter<Byte>();
+
+            buffer.WriteSerializable(value)
+                .WriteUInt32LE(extra);
+
+            ExchangeDeleteOk.Deserialize(buffer.WrittenMemory.Span, out var _, out var surplus);
+
+            Assert.Equal(expected: sizeof(UInt32), actual: surplus.Length);
+            Assert.Equal(expected: extra, actual: BitConverter.ToUInt32(surplus));
+        }
+    }
+}
--- Lapine.Core/Protocol/Commands/ExchangeDelete.cs
+++ Lapine.Core/Protocol/Commands/ExchangeDelete.cs
@@ -1,7 +1,8 @@
 namespace Lapine.Protocol.Commands {
     using System;
+    using System.Buffers;
 
-    public sealed class ExchangeDelete : ICommand {
+    public sealed class ExchangeDelete : ICommand, ISerializable {
         public (Byte ClassId, Byte MethodId) CommandId => (0x28, 0x14);
 
         public String ExchangeName { get; }
@@ -13,9 +14,35 @@ namespace Lapine.Protocol.Commands {
             IfUnused     = ifUnused;
             NoWait       = noWait;
         }
+
+        public IBufferWriter<Byte> Serialize(IBufferWriter<Byte> writer) =>
+            writer.WriteShortString(ExchangeName)
+                .WriteBits(IfUnused, NoWait);
+
+        static public Boolean Deserialize(in ReadOnlySpan<Byte> buffer, out ExchangeDelete result, out ReadOnlySpan<Byte> surplus) {
+            if (buffer.ReadShortString(out var exchangeName, out surplus) &&
+                surplus.ReadBits(out var bits, out surplus))
+            {
+                result = new ExchangeDelete(exchangeName, bits[0], bits[1]);
+                return true;
+            }
+            else {
+                result = default;
+                return false;
+            }
+        }
     }
 
-    public sealed class ExchangeDeleteOk : ICommand {
+    public sealed class ExchangeDeleteOk : ICommand, ISerializable {
         public (Byte ClassId, Byte MethodId) CommandId => (0x28, 0x15);
+
+        public IBufferWriter<Byte> Serialize(IBufferWriter<Byte> writer) =>
+            writer;
+
+        static public Boolean Deserialize(in ReadOnlySpan<Byte> buffer, out ExchangeDeleteOk result, out ReadOnlySpan<Byte> surplus) {
+            surplus = buffer;
+            result = new ExchangeDeleteOk();
+            return true;
+        }
     }
 }
