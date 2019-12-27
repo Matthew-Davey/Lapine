c691632201c4aa5a438c590777687622c290e31c
Matt Davey <matt.davey@fsfe.org>
2019-12-27 22:57:35 +0000

    Add serialization support to BasicRecover and BasicRecoverOk commands

---
+++ Lapine.Core.Tests/Protocol/Commands/BasicRecover.Tests.cs
@@ -0,0 +1,61 @@
+namespace Lapine.Protocol.Commands {
+    using System;
+    using System.Buffers;
+    using Bogus;
+    using Xunit;
+
+    public class BasicRecoverTests : Faker {
+        BasicRecover RandomSubject => new BasicRecover(
+            requeue: Random.Bool()
+        );
+
+        [Fact]
+        public void SerializationIsSymmetric() {
+            var buffer = new ArrayBufferWriter<Byte>(8);
+            var value  = RandomSubject;
+
+            value.Serialize(buffer);
+            BasicRecover.Deserialize(buffer.WrittenMemory.Span, out var deserialized, out var _);
+
+            Assert.Equal(expected: value.ReQueue, actual: deserialized.ReQueue);
+        }
+
+        [Fact]
+        public void DeserializationFailsWithInsufficientData() {
+            var result = BasicRecover.Deserialize(new Byte[0], out var _, out var _);
+
+            Assert.False(result);
+        }
+
+        [Fact]
+        public void DeserializationReturnsSurplusData() {
+            var value  = RandomSubject;
+            var extra  = Random.UInt();
+            var buffer = new ArrayBufferWriter<Byte>();
+
+            buffer.WriteSerializable(value)
+                .WriteUInt32LE(extra);
+
+            BasicRecover.Deserialize(buffer.WrittenMemory.Span, out var _, out var surplus);
+
+            Assert.Equal(expected: sizeof(UInt32), actual: surplus.Length);
+            Assert.Equal(expected: extra, actual: BitConverter.ToUInt32(surplus));
+        }
+    }
+
+    public class BasicRecoverOkTests : Faker {
+        [Fact]
+        public void DeserializationReturnsSurplusData() {
+            var extra  = Random.UInt();
+            var buffer = new ArrayBufferWriter<Byte>();
+
+            buffer.WriteSerializable(new BasicRecoverOk())
+                .WriteUInt32LE(extra);
+
+            BasicRecoverOk.Deserialize(buffer.WrittenMemory.Span, out var _, out var surplus);
+
+            Assert.Equal(expected: sizeof(UInt32), actual: surplus.Length);
+            Assert.Equal(expected: extra, actual: BitConverter.ToUInt32(surplus));
+        }
+    }
+}
--- Lapine.Core/Protocol/Commands/BasicRecover.cs
+++ Lapine.Core/Protocol/Commands/BasicRecover.cs
@@ -1,16 +1,39 @@
 namespace Lapine.Protocol.Commands {
     using System;
+    using System.Buffers;
 
-    public sealed class BasicRecover : ICommand {
+    public sealed class BasicRecover : ICommand, ISerializable {
         public (Byte ClassId, Byte MethodId) CommandId => (0x3C, 0x6E);
 
         public Boolean ReQueue { get; }
 
         public BasicRecover(Boolean requeue) =>
             ReQueue = requeue;
+
+        public IBufferWriter<Byte> Serialize(IBufferWriter<Byte> writer) =>
+            writer.WriteBoolean(ReQueue);
+
+        static public Boolean Deserialize(in ReadOnlySpan<Byte> buffer, out BasicRecover result, out ReadOnlySpan<Byte> surplus) {
+            if (buffer.ReadBoolean(out var requeue, out surplus)) {
+                result = new BasicRecover(requeue);
+                return true;
+            }
+            else {
+                result = default;
+                return false;
+            }
+        }
     }
 
-    public sealed class BasicRecoverOk : ICommand {
+    public sealed class BasicRecoverOk : ICommand, ISerializable {
         public (Byte ClassId, Byte MethodId) CommandId => (0x3C, 0x6F);
+
+        public IBufferWriter<Byte> Serialize(IBufferWriter<Byte> writer) => writer;
+
+        static public Boolean Deserialize(in ReadOnlySpan<Byte> buffer, out BasicRecoverOk result, out ReadOnlySpan<Byte> surplus) {
+            surplus = buffer;
+            result  = new BasicRecoverOk();
+            return true;
+        }
     }
 }
