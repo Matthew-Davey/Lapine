34dec9608eb14fdcaca1df18fdc3c2d20ed9ad24
Matt Davey <matt.davey@fsfe.org>
2019-12-27 22:08:02 +0000

    Add serialization support to the BasicPublish command

---
+++ Lapine.Core.Tests/Protocol/Commands/BasicPublish.Tests.cs
@@ -0,0 +1,51 @@
+namespace Lapine.Protocol.Commands {
+    using System;
+    using System.Buffers;
+    using Bogus;
+    using Xunit;
+
+    public class BasicPublishTests : Faker {
+        BasicPublish RandomSubject => new BasicPublish(
+            exchangeName: Random.Word(),
+            routingKey  : Random.Word(),
+            mandatory   : Random.Bool(),
+            immediate   : Random.Bool()
+        );
+
+        [Fact]
+        public void SerializationIsSymmetric() {
+            var buffer = new ArrayBufferWriter<Byte>(8);
+            var value  = RandomSubject;
+
+            value.Serialize(buffer);
+            BasicPublish.Deserialize(buffer.WrittenMemory.Span, out var deserialized, out var _);
+
+            Assert.Equal(expected: value.ExchangeName, actual: deserialized.ExchangeName);
+            Assert.Equal(expected: value.Immediate, actual: deserialized.Immediate);
+            Assert.Equal(expected: value.Mandatory, actual: deserialized.Mandatory);
+            Assert.Equal(expected: value.RoutingKey, actual: deserialized.RoutingKey);
+        }
+
+        [Fact]
+        public void DeserializationFailsWithInsufficientData() {
+            var result = BasicPublish.Deserialize(new Byte[0], out var _, out var _);
+
+            Assert.False(result);
+        }
+
+        [Fact]
+        public void DeserializationReturnsSurplusData() {
+            var value  = RandomSubject;
+            var extra  = Random.UInt();
+            var buffer = new ArrayBufferWriter<Byte>();
+
+            buffer.WriteSerializable(value)
+                .WriteUInt32LE(extra);
+
+            BasicPublish.Deserialize(buffer.WrittenMemory.Span, out var _, out var surplus);
+
+            Assert.Equal(expected: sizeof(UInt32), actual: surplus.Length);
+            Assert.Equal(expected: extra, actual: BitConverter.ToUInt32(surplus));
+        }
+    }
+}
--- Lapine.Core/Protocol/Commands/BasicPublish.cs
+++ Lapine.Core/Protocol/Commands/BasicPublish.cs
@@ -1,7 +1,8 @@
 namespace Lapine.Protocol.Commands {
     using System;
+    using System.Buffers;
 
-    public sealed class BasicPublish : ICommand {
+    public sealed class BasicPublish : ICommand, ISerializable {
         public (Byte ClassId, Byte MethodId) CommandId => (0x3C, 0x28);
 
         public String ExchangeName { get; }
@@ -14,6 +15,25 @@ namespace Lapine.Protocol.Commands {
             RoutingKey   = routingKey ?? throw new ArgumentNullException(nameof(routingKey));
             Mandatory    = mandatory;
             Immediate    = immediate;
+        }
+
+        public IBufferWriter<Byte> Serialize(IBufferWriter<Byte> writer) =>
+            writer.WriteShortString(ExchangeName)
+                .WriteShortString(RoutingKey)
+                .WriteBits(Mandatory, Immediate);
+
+        static public Boolean Deserialize(in ReadOnlySpan<Byte> buffer, out BasicPublish result, out ReadOnlySpan<Byte> surplus) {
+            if (buffer.ReadShortString(out var exchangeName, out surplus) &&
+                surplus.ReadShortString(out var routingKey, out surplus) &&
+                surplus.ReadBits(out var bits, out surplus))
+            {
+                result = new BasicPublish(exchangeName, routingKey, bits[0], bits[1]);
+                return true;
+            }
+            else {
+                result = default;
+                return false;
+            }
         }
     }
 }
