dbfc613ccf8dcb05364237f0b88333650e88d421
Matt Davey <matt.davey@fsfe.org>
2019-12-27 21:28:25 +0000

    Add serialization support to QueuePurge and QueuePurgeOk commands

---
+++ Lapine.Core.Tests/Protocol/Commands/QueuePurge.Tests.cs
@@ -0,0 +1,86 @@
+namespace Lapine.Protocol.Commands {
+    using System;
+    using System.Buffers;
+    using Bogus;
+    using Xunit;
+
+    public class QueuePurgeTests : Faker {
+        QueuePurge RandomSubject => new QueuePurge(
+            queueName : Random.Word(),
+            noWait    : Random.Bool()
+        );
+
+        [Fact]
+        public void SerializationIsSymmetric() {
+            var buffer = new ArrayBufferWriter<Byte>(8);
+            var value  = RandomSubject;
+
+            value.Serialize(buffer);
+            QueuePurge.Deserialize(buffer.WrittenMemory.Span, out var deserialized, out var _);
+
+            Assert.Equal(expected: value.NoWait, actual: deserialized.NoWait);
+            Assert.Equal(expected: value.QueueName, actual: deserialized.QueueName);
+        }
+
+        [Fact]
+        public void DeserializationFailsWithInsufficientData() {
+            var result = QueuePurge.Deserialize(new Byte[0], out var _, out var _);
+
+            Assert.False(result);
+        }
+
+        [Fact]
+        public void DeserializationReturnsSurplusData() {
+            var value  = RandomSubject;
+            var extra  = Random.UInt();
+            var buffer = new ArrayBufferWriter<Byte>();
+
+            buffer.WriteSerializable(value)
+                .WriteUInt32LE(extra);
+
+            QueuePurge.Deserialize(buffer.WrittenMemory.Span, out var _, out var surplus);
+
+            Assert.Equal(expected: sizeof(UInt32), actual: surplus.Length);
+            Assert.Equal(expected: extra, actual: BitConverter.ToUInt32(surplus));
+        }
+    }
+
+    public class QueuePurgeOkTests : Faker {
+        QueuePurgeOk RandomSubject => new QueuePurgeOk(
+            messageCount : Random.UInt()
+        );
+
+        [Fact]
+        public void SerializationIsSymmetric() {
+            var buffer = new ArrayBufferWriter<Byte>();
+            var value  = RandomSubject;
+
+            value.Serialize(buffer);
+            QueuePurgeOk.Deserialize(buffer.WrittenMemory.Span, out var deserialized, out var _);
+
+            Assert.Equal(expected: value.MessageCount, actual: deserialized.MessageCount);
+        }
+
+        [Fact]
+        public void DeserializationFailsWithInsufficientData() {
+            var result = QueuePurgeOk.Deserialize(new Byte[0], out var _, out var _);
+
+            Assert.False(result);
+        }
+
+        [Fact]
+        public void DeserializationReturnsSurplusData() {
+            var value  = RandomSubject;
+            var extra  = Random.UInt();
+            var buffer = new ArrayBufferWriter<Byte>();
+
+            buffer.WriteSerializable(value)
+                .WriteUInt32LE(extra);
+
+            QueuePurgeOk.Deserialize(buffer.WrittenMemory.Span, out var _, out var surplus);
+
+            Assert.Equal(expected: sizeof(UInt32), actual: surplus.Length);
+            Assert.Equal(expected: extra, actual: BitConverter.ToUInt32(surplus));
+        }
+    }
+}
--- Lapine.Core/Protocol/Commands/QueuePurge.cs
+++ Lapine.Core/Protocol/Commands/QueuePurge.cs
@@ -1,7 +1,8 @@
 namespace Lapine.Protocol.Commands {
     using System;
+    using System.Buffers;
 
-    public sealed class QueuePurge : ICommand {
+    public sealed class QueuePurge : ICommand, ISerializable {
         public (Byte ClassId, Byte MethodId) CommandId => (0x32, 0x1E);
 
         public String QueueName { get; }
@@ -11,14 +12,45 @@ namespace Lapine.Protocol.Commands {
             QueueName = queueName ?? throw new ArgumentNullException(nameof(queueName));
             NoWait    = noWait;
         }
+
+        public IBufferWriter<Byte> Serialize(IBufferWriter<Byte> writer) =>
+            writer.WriteShortString(QueueName)
+                .WriteBoolean(NoWait);
+
+        static public Boolean Deserialize(in ReadOnlySpan<Byte> buffer, out QueuePurge result, out ReadOnlySpan<Byte> surplus) {
+            if (buffer.ReadShortString(out var queueName, out surplus) &&
+                surplus.ReadBoolean(out var noWait, out surplus))
+            {
+                result = new QueuePurge(queueName, noWait);
+                return true;
+            }
+            else {
+                result = default;
+                return false;
+            }
+        }
     }
 
-    public sealed class QueuePurgeOk : ICommand {
+    public sealed class QueuePurgeOk : ICommand, ISerializable {
         public (Byte ClassId, Byte MethodId) CommandId => (0x32, 0x1F);
 
         public UInt32 MessageCount { get; }
 
         public QueuePurgeOk(UInt32 messageCount) =>
             MessageCount = messageCount;
+
+        public IBufferWriter<Byte> Serialize(IBufferWriter<Byte> writer) =>
+            writer.WriteUInt32BE(MessageCount);
+
+        static public Boolean Deserialize(in ReadOnlySpan<Byte> buffer, out QueuePurgeOk result, out ReadOnlySpan<Byte> surplus) {
+            if (buffer.ReadUInt32BE(out var messageCount, out surplus)) {
+                result = new QueuePurgeOk(messageCount);
+                return true;
+            }
+            else {
+                result = default;
+                return false;
+            }
+        }
     }
 }
