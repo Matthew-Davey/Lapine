f0c37be4e7ce9cc96dcc2745825441e0ed4ef7e2
Matt Davey <matt.davey@fsfe.org>
2019-12-27 00:14:22 +0000

    Add serialization support to ChannelClose command

---
+++ Lapine.Core.Tests/Protocol/Commands/ChannelClose.Tests.cs
@@ -0,0 +1,49 @@
+namespace Lapine.Protocol.Commands {
+    using System;
+    using System.Buffers;
+    using Bogus;
+    using Xunit;
+
+    public class ChannelCloseTests : Faker {
+        ChannelClose RandomSubject => new ChannelClose(
+            replyCode    : Random.UShort(),
+            replyText    : Lorem.Sentence(),
+            failingMethod: (Random.UShort(), Random.UShort())
+        );
+
+        [Fact]
+        public void SerializationIsSymmetric() {
+            var buffer = new ArrayBufferWriter<Byte>();
+            var value  = RandomSubject;
+
+            value.Serialize(buffer);
+            ChannelClose.Deserialize(buffer.WrittenMemory.Span, out var deserialized, out var _);
+
+            Assert.Equal(expected: value.FailingMethod, actual: deserialized.FailingMethod);
+            Assert.Equal(expected: value.ReplyCode, actual: deserialized.ReplyCode);
+            Assert.Equal(expected: value.ReplyText, actual: deserialized.ReplyText);
+        }
+
+        [Fact]
+        public void DeserializationFailsWithInsufficientData() {
+            var result = ChannelClose.Deserialize(new Byte[0], out var _, out var _);
+
+            Assert.False(result);
+        }
+
+        [Fact]
+        public void DeserializationReturnsSurplusData() {
+            var value  = RandomSubject;
+            var extra  = Random.UInt();
+            var buffer = new ArrayBufferWriter<Byte>();
+
+            buffer.WriteSerializable(value)
+                .WriteUInt32LE(extra);
+
+            ChannelClose.Deserialize(buffer.WrittenMemory.Span, out var _, out var surplus);
+
+            Assert.Equal(expected: sizeof(UInt32), actual: surplus.Length);
+            Assert.Equal(expected: extra, actual: BitConverter.ToUInt32(surplus));
+        }
+    }
+}
--- Lapine.Core/Protocol/Commands/ChannelClose.cs
+++ Lapine.Core/Protocol/Commands/ChannelClose.cs
@@ -1,15 +1,40 @@
 namespace Lapine.Protocol.Commands {
     using System;
+    using System.Buffers;
 
-    public sealed class ChannelClose : ICommand {
+    public sealed class ChannelClose : ICommand, ISerializable {
         public (Byte ClassId, Byte MethodId) CommandId => (0x14, 0x28);
 
         public UInt16 ReplyCode { get; }
         public String ReplyText { get; }
 
-        public ChannelClose(UInt16 replyCode, String replyText) {
-            ReplyCode = replyCode;
-            ReplyText = replyText ?? throw new ArgumentNullException(nameof(replyText));
+        public (UInt16 ClassId, UInt16 MethodId) FailingMethod { get; }
+
+        public ChannelClose(UInt16 replyCode, String replyText, (UInt16 ClassId, UInt16 MethodId) failingMethod) {
+            ReplyCode     = replyCode;
+            ReplyText     = replyText ?? throw new ArgumentNullException(nameof(replyText));
+            FailingMethod = failingMethod;
+        }
+
+        public IBufferWriter<Byte> Serialize(IBufferWriter<Byte> writer) =>
+            writer.WriteUInt16BE(ReplyCode)
+                .WriteShortString(ReplyText)
+                .WriteUInt16BE(FailingMethod.ClassId)
+                .WriteUInt16BE(FailingMethod.MethodId);
+
+        static public Boolean Deserialize(in ReadOnlySpan<Byte> buffer, out ChannelClose result, out ReadOnlySpan<Byte> surplus) {
+            if (buffer.ReadUInt16BE(out var replyCode, out surplus) &&
+                surplus.ReadShortString(out var replyText, out surplus) &&
+                surplus.ReadUInt16BE(out var classId, out surplus) &&
+                surplus.ReadUInt16BE(out var methodId, out surplus))
+            {
+                result = new ChannelClose(replyCode, replyText, (classId, methodId));
+                return true;
+            }
+            else {
+                result = default;
+                return false;
+            }
         }
     }
 
