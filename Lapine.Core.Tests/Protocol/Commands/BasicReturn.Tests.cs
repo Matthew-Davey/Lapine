0528aaea5b49f9e14a842d7a18118b2592514261
Matt Davey <matt.davey@fsfe.org>
2019-12-27 22:13:53 +0000

    Add serialization support to the BasicReturn command

---
+++ Lapine.Core.Tests/Protocol/Commands/BasicReturn.Tests.cs
@@ -0,0 +1,51 @@
+namespace Lapine.Protocol.Commands {
+    using System;
+    using System.Buffers;
+    using Bogus;
+    using Xunit;
+
+    public class BasicReturnTests : Faker {
+        BasicReturn RandomSubject => new BasicReturn(
+            replyCode   : Random.UShort(),
+            replyText   : Random.Word(),
+            exchangeName: Random.Word(),
+            routingKey  : Random.Word()
+        );
+
+        [Fact]
+        public void SerializationIsSymmetric() {
+            var buffer = new ArrayBufferWriter<Byte>(8);
+            var value  = RandomSubject;
+
+            value.Serialize(buffer);
+            BasicReturn.Deserialize(buffer.WrittenMemory.Span, out var deserialized, out var _);
+
+            Assert.Equal(expected: value.ExchangeName, actual: deserialized.ExchangeName);
+            Assert.Equal(expected: value.ReplyCode, actual: deserialized.ReplyCode);
+            Assert.Equal(expected: value.ReplyText, actual: deserialized.ReplyText);
+            Assert.Equal(expected: value.RoutingKey, actual: deserialized.RoutingKey);
+        }
+
+        [Fact]
+        public void DeserializationFailsWithInsufficientData() {
+            var result = BasicReturn.Deserialize(new Byte[0], out var _, out var _);
+
+            Assert.False(result);
+        }
+
+        [Fact]
+        public void DeserializationReturnsSurplusData() {
+            var value  = RandomSubject;
+            var extra  = Random.UInt();
+            var buffer = new ArrayBufferWriter<Byte>();
+
+            buffer.WriteSerializable(value)
+                .WriteUInt32LE(extra);
+
+            BasicReturn.Deserialize(buffer.WrittenMemory.Span, out var _, out var surplus);
+
+            Assert.Equal(expected: sizeof(UInt32), actual: surplus.Length);
+            Assert.Equal(expected: extra, actual: BitConverter.ToUInt32(surplus));
+        }
+    }
+}
--- Lapine.Core/Protocol/Commands/BasicReturn.cs
+++ Lapine.Core/Protocol/Commands/BasicReturn.cs
@@ -1,7 +1,8 @@
 namespace Lapine.Protocol.Commands {
     using System;
+    using System.Buffers;
 
-    public sealed class BasicReturn : ICommand {
+    public sealed class BasicReturn : ICommand, ISerializable {
         public (Byte ClassId, Byte MethodId) CommandId => (0x3C, 0x32);
 
         public UInt16 ReplyCode { get; }
@@ -14,6 +15,27 @@ namespace Lapine.Protocol.Commands {
             ReplyText    = replyText;
             ExchangeName = exchangeName;
             RoutingKey   = routingKey;
+        }
+
+        public IBufferWriter<Byte> Serialize(IBufferWriter<Byte> writer) =>
+            writer.WriteUInt16BE(ReplyCode)
+                .WriteShortString(ReplyText)
+                .WriteShortString(ExchangeName)
+                .WriteShortString(RoutingKey);
+
+        static public Boolean Deserialize(in ReadOnlySpan<Byte> buffer, out BasicReturn result, out ReadOnlySpan<Byte> surplus) {
+            if (buffer.ReadUInt16BE(out var replyCode, out surplus) &&
+                surplus.ReadShortString(out var replyText, out surplus) &&
+                surplus.ReadShortString(out var exchangeName, out surplus) &&
+                surplus.ReadShortString(out var routingKey, out surplus))
+            {
+                result = new BasicReturn(replyCode, replyText, exchangeName, routingKey);
+                return true;
+            }
+            else {
+                result = default;
+                return false;
+            }
         }
     }
 }
