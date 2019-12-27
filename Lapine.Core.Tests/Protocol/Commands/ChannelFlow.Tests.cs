e0b089bafc893ef144a190feeb62b714420bf362
Matt Davey <matt.davey@fsfe.org>
2019-12-27 00:04:15 +0000

    Add serialization support to ChannelFlow command

---
+++ Lapine.Core.Tests/Protocol/Commands/ChannelFlow.Tests.cs
@@ -0,0 +1,45 @@
+namespace Lapine.Protocol.Commands {
+    using System;
+    using System.Buffers;
+    using Bogus;
+    using Xunit;
+
+    public class ChannelFlowTests : Faker {
+        ChannelFlow RandomSubject => new ChannelFlow(
+            active: Random.Bool()
+        );
+
+        [Fact]
+        public void SerializationIsSymmetric() {
+            var buffer = new ArrayBufferWriter<Byte>();
+            var value  = RandomSubject;
+
+            value.Serialize(buffer);
+            ChannelFlow.Deserialize(buffer.WrittenMemory.Span, out var deserialized, out var _);
+
+            Assert.Equal(expected: value.Active, actual: deserialized.Active);
+        }
+
+        [Fact]
+        public void DeserializationFailsWithInsufficientData() {
+            var result = ChannelFlow.Deserialize(new Byte[0], out var _, out var _);
+
+            Assert.False(result);
+        }
+
+        [Fact]
+        public void DeserializationReturnsSurplusData() {
+            var value  = RandomSubject;
+            var extra  = Random.UInt();
+            var buffer = new ArrayBufferWriter<Byte>();
+
+            buffer.WriteSerializable(value)
+                .WriteUInt32LE(extra);
+
+            ChannelFlow.Deserialize(buffer.WrittenMemory.Span, out var _, out var surplus);
+
+            Assert.Equal(expected: sizeof(UInt32), actual: surplus.Length);
+            Assert.Equal(expected: extra, actual: BitConverter.ToUInt32(surplus));
+        }
+    }
+}
--- Lapine.Core/Protocol/Commands/ChannelFlow.cs
+++ Lapine.Core/Protocol/Commands/ChannelFlow.cs
@@ -1,13 +1,28 @@
 namespace Lapine.Protocol.Commands {
     using System;
+    using System.Buffers;
 
-    public sealed class ChannelFlow : ICommand {
+    public sealed class ChannelFlow : ICommand, ISerializable {
         public (Byte ClassId, Byte MethodId) CommandId => (0x14, 0x14);
 
         public Boolean Active { get; }
 
         public ChannelFlow(Boolean active) =>
             Active = active;
+
+        public IBufferWriter<Byte> Serialize(IBufferWriter<Byte> writer) =>
+            writer.WriteBoolean(Active);
+
+        static public Boolean Deserialize(in ReadOnlySpan<Byte> buffer, out ChannelFlow result, out ReadOnlySpan<Byte> surplus) {
+            if (buffer.ReadBoolean(out var active, out surplus)) {
+                result = new ChannelFlow(active);
+                return true;
+            }
+            else {
+                result = default;
+                return false;
+            }
+        }
     }
 
     public sealed class ChannelFlowOk : ICommand {
