da026431cb404ffec1282faa4da858663bb3335c
Matt Davey <matt.davey@fsfe.org>
2019-12-27 21:37:17 +0000

    Add serialization support to QueueDelete and QueueDeleteOk commands

---
+++ Lapine.Core.Tests/Protocol/Commands/QueueDelete.Tests.cs
@@ -0,0 +1,90 @@
+namespace Lapine.Protocol.Commands {
+    using System;
+    using System.Buffers;
+    using Bogus;
+    using Xunit;
+
+    public class QueueDeleteTests : Faker {
+        QueueDelete RandomSubject => new QueueDelete(
+            queueName : Random.Word(),
+            ifUnused  : Random.Bool(),
+            ifEmpty   : Random.Bool(),
+            noWait    : Random.Bool()
+        );
+
+        [Fact]
+        public void SerializationIsSymmetric() {
+            var buffer = new ArrayBufferWriter<Byte>(8);
+            var value  = RandomSubject;
+
+            value.Serialize(buffer);
+            QueueDelete.Deserialize(buffer.WrittenMemory.Span, out var deserialized, out var _);
+
+            Assert.Equal(expected: value.IfEmpty, actual: deserialized.IfEmpty);
+            Assert.Equal(expected: value.IfUnused, actual: deserialized.IfUnused);
+            Assert.Equal(expected: value.NoWait, actual: deserialized.NoWait);
+            Assert.Equal(expected: value.QueueName, actual: deserialized.QueueName);
+        }
+
+        [Fact]
+        public void DeserializationFailsWithInsufficientData() {
+            var result = QueueDelete.Deserialize(new Byte[0], out var _, out var _);
+
+            Assert.False(result);
+        }
+
+        [Fact]
+        public void DeserializationReturnsSurplusData() {
+            var value  = RandomSubject;
+            var extra  = Random.UInt();
+            var buffer = new ArrayBufferWriter<Byte>();
+
+            buffer.WriteSerializable(value)
+                .WriteUInt32LE(extra);
+
+            QueueDelete.Deserialize(buffer.WrittenMemory.Span, out var _, out var surplus);
+
+            Assert.Equal(expected: sizeof(UInt32), actual: surplus.Length);
+            Assert.Equal(expected: extra, actual: BitConverter.ToUInt32(surplus));
+        }
+    }
+
+    public class QueueDeleteOkTests : Faker {
+        QueueDeleteOk RandomSubject => new QueueDeleteOk(
+            messageCount : Random.UInt()
+        );
+
+        [Fact]
+        public void SerializationIsSymmetric() {
+            var buffer = new ArrayBufferWriter<Byte>();
+            var value  = RandomSubject;
+
+            value.Serialize(buffer);
+            QueueDeleteOk.Deserialize(buffer.WrittenMemory.Span, out var deserialized, out var _);
+
+            Assert.Equal(expected: value.MessageCount, actual: deserialized.MessageCount);
+        }
+
+        [Fact]
+        public void DeserializationFailsWithInsufficientData() {
+            var result = QueueDeleteOk.Deserialize(new Byte[0], out var _, out var _);
+
+            Assert.False(result);
+        }
+
+        [Fact]
+        public void DeserializationReturnsSurplusData() {
+            var value  = RandomSubject;
+            var extra  = Random.UInt();
+            var buffer = new ArrayBufferWriter<Byte>();
+
+            buffer.WriteSerializable(value)
+                .WriteUInt32LE(extra);
+
+            QueueDeleteOk.Deserialize(buffer.WrittenMemory.Span, out var _, out var surplus);
+
+            Assert.Equal(expected: sizeof(UInt32), actual: surplus.Length);
+            Assert.Equal(expected: extra, actual: BitConverter.ToUInt32(surplus));
+        }
+    }
+}
--- Lapine.Core/Protocol/Commands/QueueDelete.cs
+++ Lapine.Core/Protocol/Commands/QueueDelete.cs
@@ -1,7 +1,8 @@
 namespace Lapine.Protocol.Commands {
     using System;
+    using System.Buffers;
 
-    public sealed class QueueDelete : ICommand {
+    public sealed class QueueDelete : ICommand, ISerializable {
         public (Byte ClassId, Byte MethodId) CommandId => (0x32, 0x28);
 
         public String QueueName { get; }
@@ -9,20 +10,51 @@ namespace Lapine.Protocol.Commands {
         public Boolean IfEmpty { get; }
         public Boolean NoWait { get; }
 
-        public QueueDelete(String queueName, Boolean ifInused, Boolean ifEmpty, Boolean noWait) {
+        public QueueDelete(String queueName, Boolean ifUnused, Boolean ifEmpty, Boolean noWait) {
             QueueName = queueName ?? throw new ArgumentNullException(nameof(queueName));
-            IfUnused  = IfUnused;
+            IfUnused  = ifUnused;
             IfEmpty   = ifEmpty;
             NoWait    = noWait;
         }
+
+        public IBufferWriter<Byte> Serialize(IBufferWriter<Byte> writer) =>
+            writer.WriteShortString(QueueName)
+                .WriteBits(IfUnused, IfEmpty, NoWait);
+
+        static public Boolean Deserialize(in ReadOnlySpan<Byte> buffer, out QueueDelete result, out ReadOnlySpan<Byte> surplus) {
+            if (buffer.ReadShortString(out var queueName, out surplus) &&
+                surplus.ReadBits(out var bits, out surplus))
+            {
+                result = new QueueDelete(queueName, bits[0], bits[1], bits[2]);
+                return true;
+            }
+            else {
+                result = default;
+                return false;
+            }
+        }
     }
 
-    public sealed class QueueDeleteOk : ICommand {
+    public sealed class QueueDeleteOk : ICommand, ISerializable {
         public (Byte ClassId, Byte MethodId) CommandId => (0x32, 0x29);
 
         public UInt32 MessageCount { get; }
 
         public QueueDeleteOk(UInt32 messageCount) =>
             MessageCount = messageCount;
+
+        public IBufferWriter<Byte> Serialize(IBufferWriter<Byte> writer) =>
+            writer.WriteUInt32BE(MessageCount);
+
+        static public Boolean Deserialize(in ReadOnlySpan<Byte> buffer, out QueueDeleteOk result, out ReadOnlySpan<Byte> surplus) {
+            if (buffer.ReadUInt32BE(out var messageCount, out surplus)) {
+                result = new QueueDeleteOk(messageCount);
+                return true;
+            }
+            else {
+                result = default;
+                return false;
+            }
+        }
     }
 }
