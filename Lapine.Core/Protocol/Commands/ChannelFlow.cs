9ab227dd89c7cde81e155a9653ba567e8d3da39e
Matt Davey <matt.davey@fsfe.org>
2019-12-27 00:05:33 +0000

    Add serialization support to ChannelFlowOk command

--- Lapine.Core/Protocol/Commands/ChannelFlow.cs
+++ Lapine.Core/Protocol/Commands/ChannelFlow.cs
@@ -25,12 +25,26 @@ namespace Lapine.Protocol.Commands {
         }
     }
 
-    public sealed class ChannelFlowOk : ICommand {
+    public sealed class ChannelFlowOk : ICommand, ISerializable {
         public (Byte ClassId, Byte MethodId) CommandId => (0x14, 0x15);
 
         public Boolean Active { get; }
 
         public ChannelFlowOk(Boolean active) =>
             Active = active;
+
+        public IBufferWriter<Byte> Serialize(IBufferWriter<Byte> writer) =>
+            writer.WriteBoolean(Active);
+
+        static public Boolean Deserialize(in ReadOnlySpan<Byte> buffer, out ChannelFlowOk result, out ReadOnlySpan<Byte> surplus) {
+            if (buffer.ReadBoolean(out var active, out surplus)) {
+                result = new ChannelFlowOk(active);
+                return true;
+            }
+            else {
+                result = default;
+                return false;
+            }
+        }
     }
 }
