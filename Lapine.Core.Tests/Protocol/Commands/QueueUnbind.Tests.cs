2b4f0e5c768790b22fec092f432c8c1e473c5352
Matt Davey <matt.davey@fsfe.org>
2019-12-27 03:10:18 +0000

    Add serialization support to QueueUnbind and QueueUnbindOk commands

---
+++ Lapine.Core.Tests/Protocol/Commands/QueueUnbind.Tests.cs
@@ -0,0 +1,70 @@
+namespace Lapine.Protocol.Commands {
+    using System;
+    using System.Buffers;
+    using System.Collections.Generic;
+    using System.Linq;
+    using Bogus;
+    using Xunit;
+
+    public class QueueUnbindTests : Faker {
+        QueueUnbind RandomSubject => new QueueUnbind(
+            queueName   : Random.Word(),
+            exchangeName: Random.Word(),
+            routingKey  : Random.Word(),
+            arguments   : new Dictionary<String, Object> {{ Random.Word(), Random.UInt() }}
+        );
+
+        [Fact]
+        public void SerializationIsSymmetric() {
+            var buffer = new ArrayBufferWriter<Byte>(8);
+            var value  = RandomSubject;
+
+            value.Serialize(buffer);
+            QueueUnbind.Deserialize(buffer.WrittenMemory.Span, out var deserialized, out var _);
+
+            Assert.Equal(expected: value.Arguments.ToList(), actual: deserialized.Arguments.ToList());
+            Assert.Equal(expected: value.ExchangeName, actual: deserialized.ExchangeName);
+            Assert.Equal(expected: value.QueueName, actual: deserialized.QueueName);
+            Assert.Equal(expected: value.RoutingKey, actual: deserialized.RoutingKey);
+        }
+
+        [Fact]
+        public void DeserializationFailsWithInsufficientData() {
+            var result = QueueUnbind.Deserialize(new Byte[0], out var _, out var _);
+
+            Assert.False(result);
+        }
+
+        [Fact]
+        public void DeserializationReturnsSurplusData() {
+            var value  = RandomSubject;
+            var extra  = Random.UInt();
+            var buffer = new ArrayBufferWriter<Byte>();
+
+            buffer.WriteSerializable(value)
+                .WriteUInt32LE(extra);
+
+            QueueUnbind.Deserialize(buffer.WrittenMemory.Span, out var _, out var surplus);
+
+            Assert.Equal(expected: sizeof(UInt32), actual: surplus.Length);
+            Assert.Equal(expected: extra, actual: BitConverter.ToUInt32(surplus));
+        }
+    }
+
+    public class QueueUnbindOkTests : Faker {
+        [Fact]
+        public void DeserializationReturnsSurplusData() {
+            var value  = new QueueUnbindOk();
+            var extra  = Random.UInt();
+            var buffer = new ArrayBufferWriter<Byte>();
+
+            buffer.WriteSerializable(value)
+                .WriteUInt32LE(extra);
+
+            QueueUnbindOk.Deserialize(buffer.WrittenMemory.Span, out var _, out var surplus);
+
+            Assert.Equal(expected: sizeof(UInt32), actual: surplus.Length);
+            Assert.Equal(expected: extra, actual: BitConverter.ToUInt32(surplus));
+        }
+    }
+}
--- Lapine.Core/Protocol/Commands/QueueUnbind.cs
+++ Lapine.Core/Protocol/Commands/QueueUnbind.cs
@@ -1,24 +1,54 @@
 namespace Lapine.Protocol.Commands {
     using System;
+    using System.Buffers;
     using System.Collections.Generic;
 
-    public sealed class QueueUnbind : ICommand {
+    public sealed class QueueUnbind : ICommand, ISerializable {
         public (Byte ClassId, Byte MethodId) CommandId => (0x32, 0x32);
 
         public String QueueName { get; }
         public String ExchangeName { get; }
         public String RoutingKey { get; }
-        public IDictionary<String, Object> Arguments { get; }
+        public IReadOnlyDictionary<String, Object> Arguments { get; }
 
-        public QueueUnbind(String queueName, String exchangeName, String routingKey, IDictionary<String, Object> arguments) {
+        public QueueUnbind(String queueName, String exchangeName, String routingKey, IReadOnlyDictionary<String, Object> arguments) {
             QueueName    = queueName ?? throw new ArgumentNullException(nameof(queueName));
             ExchangeName = exchangeName ?? throw new ArgumentNullException(nameof(exchangeName));
             RoutingKey   = routingKey ?? throw new ArgumentNullException(nameof(routingKey));
             Arguments    = arguments ?? throw new ArgumentNullException(nameof(arguments));
         }
+
+        public IBufferWriter<Byte> Serialize(IBufferWriter<Byte> writer) =>
+            writer.WriteShortString(QueueName)
+                .WriteShortString(ExchangeName)
+                .WriteShortString(RoutingKey)
+                .WriteFieldTable(Arguments);
+
+        static public Boolean Deserialize(in ReadOnlySpan<Byte> buffer, out QueueUnbind result, out ReadOnlySpan<Byte> surplus) {
+            if (buffer.ReadShortString(out var queueName, out surplus) &&
+                surplus.ReadShortString(out var exchangeName, out surplus) &&
+                surplus.ReadShortString(out var routingKey, out surplus) &&
+                surplus.ReadFieldTable(out var arguments, out surplus))
+            {
+                result = new QueueUnbind(queueName, exchangeName, routingKey, arguments);
+                return true;
+            }
+            else {
+                result = default;
+                return false;
+            }
+        }
     }
 
-    public sealed class QueueUnbindOk : ICommand {
+    public sealed class QueueUnbindOk : ICommand, ISerializable {
         public (Byte ClassId, Byte MethodId) CommandId => (0x32, 0x33);
+
+        public IBufferWriter<Byte> Serialize(IBufferWriter<Byte> writer) => writer;
+
+        static public Boolean Deserialize(in ReadOnlySpan<Byte> buffer, out QueueUnbindOk result, out ReadOnlySpan<Byte> surplus) {
+            result = new QueueUnbindOk();
+            surplus = buffer;
+            return true;
+        }
     }
 }
