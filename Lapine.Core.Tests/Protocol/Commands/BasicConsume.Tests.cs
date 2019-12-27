dfc5017801d2fe8b55a574c651fc9509d1b2b842
Matt Davey <matt.davey@fsfe.org>
2019-12-27 21:57:07 +0000

    Add serialization support to BasicConsume and BasicConsumeOk commands

---
+++ Lapine.Core.Tests/Protocol/Commands/BasicConsume.Tests.cs
@@ -0,0 +1,98 @@
+namespace Lapine.Protocol.Commands {
+    using System;
+    using System.Buffers;
+    using System.Collections.Generic;
+    using System.Linq;
+    using Bogus;
+    using Xunit;
+
+    public class BasicConsumeTests : Faker {
+        BasicConsume RandomSubject => new BasicConsume(
+            queueName   : Random.Word(),
+            consumerTag : Random.Word(),
+            noLocal     : Random.Bool(),
+            noAck       : Random.Bool(),
+            exclusive   : Random.Bool(),
+            noWait      : Random.Bool(),
+            arguments   : new Dictionary<String, Object> {{ Random.Word(), Random.UInt() }}
+        );
+
+        [Fact]
+        public void SerializationIsSymmetric() {
+            var buffer = new ArrayBufferWriter<Byte>(8);
+            var value  = RandomSubject;
+
+            value.Serialize(buffer);
+            BasicConsume.Deserialize(buffer.WrittenMemory.Span, out var deserialized, out var _);
+
+            Assert.Equal(expected: value.Arguments.ToList(), actual: deserialized.Arguments.ToList());
+            Assert.Equal(expected: value.ConsumerTag, actual: deserialized.ConsumerTag);
+            Assert.Equal(expected: value.Exclusive, actual: deserialized.Exclusive);
+            Assert.Equal(expected: value.NoAck, actual: deserialized.NoAck);
+            Assert.Equal(expected: value.NoLocal, actual: deserialized.NoLocal);
+            Assert.Equal(expected: value.NoWait, actual: deserialized.NoWait);
+            Assert.Equal(expected: value.QueueName, actual: deserialized.QueueName);
+        }
+
+        [Fact]
+        public void DeserializationFailsWithInsufficientData() {
+            var result = BasicConsume.Deserialize(new Byte[0], out var _, out var _);
+
+            Assert.False(result);
+        }
+
+        [Fact]
+        public void DeserializationReturnsSurplusData() {
+            var value  = RandomSubject;
+            var extra  = Random.UInt();
+            var buffer = new ArrayBufferWriter<Byte>();
+
+            buffer.WriteSerializable(value)
+                .WriteUInt32LE(extra);
+
+            BasicConsume.Deserialize(buffer.WrittenMemory.Span, out var _, out var surplus);
+
+            Assert.Equal(expected: sizeof(UInt32), actual: surplus.Length);
+            Assert.Equal(expected: extra, actual: BitConverter.ToUInt32(surplus));
+        }
+    }
+
+    public class BasicConsumeOkTests : Faker {
+        BasicConsumeOk RandomSubject => new BasicConsumeOk(
+            consumerTag: Random.Word()
+        );
+
+        [Fact]
+        public void SerializationIsSymmetric() {
+            var buffer = new ArrayBufferWriter<Byte>();
+            var value  = RandomSubject;
+
+            value.Serialize(buffer);
+            BasicConsumeOk.Deserialize(buffer.WrittenMemory.Span, out var deserialized, out var _);
+
+            Assert.Equal(expected: value.ConsumerTag, actual: deserialized.ConsumerTag);
+        }
+
+        [Fact]
+        public void DeserializationFailsWithInsufficientData() {
+            var result = BasicConsumeOk.Deserialize(new Byte[0], out var _, out var _);
+
+            Assert.False(result);
+        }
+
+        [Fact]
+        public void DeserializationReturnsSurplusData() {
+            var value  = RandomSubject;
+            var extra  = Random.UInt();
+            var buffer = new ArrayBufferWriter<Byte>();
+
+            buffer.WriteSerializable(value)
+                .WriteUInt32LE(extra);
+
+            BasicConsumeOk.Deserialize(buffer.WrittenMemory.Span, out var _, out var surplus);
+
+            Assert.Equal(expected: sizeof(UInt32), actual: surplus.Length);
+            Assert.Equal(expected: extra, actual: BitConverter.ToUInt32(surplus));
+        }
+    }
+}
--- Lapine.Core/Protocol/Commands/BasicConsume.cs
+++ Lapine.Core/Protocol/Commands/BasicConsume.cs
@@ -1,8 +1,9 @@
 namespace Lapine.Protocol.Commands {
     using System;
+    using System.Buffers;
     using System.Collections.Generic;
 
-    public sealed class BasicConsume : ICommand {
+    public sealed class BasicConsume : ICommand, ISerializable {
         public (Byte ClassId, Byte MethodId) CommandId => (0x3C, 0x14);
 
         public String QueueName { get; }
@@ -11,9 +12,9 @@ namespace Lapine.Protocol.Commands {
         public Boolean NoAck { get; }
         public Boolean Exclusive { get; }
         public Boolean NoWait { get; }
-        public IDictionary<String, Object> Arguments { get; }
+        public IReadOnlyDictionary<String, Object> Arguments { get; }
 
-        public BasicConsume(String queueName, String consumerTag, Boolean noLocal, Boolean noAck, Boolean exclusive, Boolean noWait, IDictionary<String, Object> arguments) {
+        public BasicConsume(String queueName, String consumerTag, Boolean noLocal, Boolean noAck, Boolean exclusive, Boolean noWait, IReadOnlyDictionary<String, Object> arguments) {
             QueueName   = queueName ?? throw new ArgumentNullException(nameof(queueName));
             ConsumerTag = consumerTag ?? throw new ArgumentNullException(nameof(consumerTag));
             NoLocal     = noLocal;
@@ -22,14 +23,49 @@ namespace Lapine.Protocol.Commands {
             NoWait      = noWait;
             Arguments   = arguments;
         }
+
+        public IBufferWriter<Byte> Serialize(IBufferWriter<Byte> writer) =>
+            writer.WriteShortString(QueueName)
+                .WriteShortString(ConsumerTag)
+                .WriteBits(NoLocal, NoAck, Exclusive, NoWait)
+                .WriteFieldTable(Arguments);
+
+        static public Boolean Deserialize(in ReadOnlySpan<Byte> buffer, out BasicConsume result, out ReadOnlySpan<Byte> surplus) {
+            if (buffer.ReadShortString(out var queueName, out surplus) &&
+                surplus.ReadShortString(out var consumerTag, out surplus) &&
+                surplus.ReadBits(out var bits, out surplus) &&
+                surplus.ReadFieldTable(out var arguments, out surplus))
+            {
+                result = new BasicConsume(queueName, consumerTag, bits[0], bits[1], bits[2], bits[3], arguments);
+                return true;
+            }
+            else {
+                result = default;
+                return false;
+            }
+        }
     }
 
-    public sealed class BasicConsumeOk : ICommand {
+    public sealed class BasicConsumeOk : ICommand, ISerializable {
         public (Byte ClassId, Byte MethodId) CommandId => (0x3C, 0x15);
 
         public String ConsumerTag { get; }
 
         public BasicConsumeOk(String consumerTag) =>
             ConsumerTag = consumerTag ?? throw new ArgumentNullException(nameof(consumerTag));
+
+        public IBufferWriter<Byte> Serialize(IBufferWriter<Byte> writer) =>
+            writer.WriteShortString(ConsumerTag);
+
+        static public Boolean Deserialize(in ReadOnlySpan<Byte> buffer, out BasicConsumeOk result, out ReadOnlySpan<Byte> surplus) {
+            if (buffer.ReadShortString(out var consumerTag, out surplus)) {
+                result = new BasicConsumeOk(consumerTag);
+                return true;
+            }
+            else {
+                result = default;
+                return false;
+            }
+        }
     }
 }
