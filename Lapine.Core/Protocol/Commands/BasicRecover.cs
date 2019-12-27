5d59a772281eb1c02cbdd4c0bfd1d5dab6c737dc
Matt Davey <matt.davey@fsfe.org>
2019-12-27 22:50:59 +0000

    Remove deprecated BasicRecoverAsync command

--- Lapine.Core/Protocol/Commands/BasicRecover.cs
+++ Lapine.Core/Protocol/Commands/BasicRecover.cs
@@ -1,15 +1,6 @@
 namespace Lapine.Protocol.Commands {
     using System;
 
-    public sealed class BasicRecoverAsync : ICommand {
-        public (Byte ClassId, Byte MethodId) CommandId => (0x3C, 0x64);
-
-        public Boolean ReQueue { get; }
-
-        public BasicRecoverAsync(Boolean requeue) =>
-            ReQueue = requeue;
-    }
-
     public sealed class BasicRecover : ICommand {
         public (Byte ClassId, Byte MethodId) CommandId => (0x3C, 0x6E);
 
