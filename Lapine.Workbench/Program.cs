2b6746f465c644e2d1c429339ab7d3bb9d412071
Matt Davey <matt.davey@fsfe.org>
2019-12-29 01:46:14 +0000

    Add Workbench project to run ad-hoc tests against the Vagrant RabbitMQ instance

---
+++ Lapine.Workbench/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace Lapine.Workbench {
+    using System;
+    using System.Net;
+    using System.Threading;
+    using Lapine.Agents;
+    using Lapine.Agents.Commands;
+    using Lapine.Agents.Middleware;
+    using Microsoft.Extensions.Logging;
+    using Proto;
+
+    class Program {
+        static void Main() {
+            Lapine.Log.LoggerFactory = LoggerFactory.Create(config => {
+                config.AddConsole();
+                config.SetMinimumLevel(LogLevel.Debug);
+            });
+            Proto.Log.SetLoggerFactory(Lapine.Log.LoggerFactory);
+
+            var resetEvent = new ManualResetEventSlim();
+
+            Console.CancelKeyPress += (_, args) => {
+                args.Cancel = true;
+                resetEvent.Set();
+            };
+
+            var context = new RootContext();
+            var socketAgent = context.SpawnNamed(
+                Props.FromProducer(() => new SocketAgent())
+                    .WithContextDecorator(context => new LoggingContextDecorator(context)),
+                "socket"
+            );
+
+            context.Send(socketAgent, new SocketConnect(IPAddress.Loopback, 5672));
+
+            resetEvent.Wait();
+        }
+    }
+}
