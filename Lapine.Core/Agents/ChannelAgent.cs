b2ac27f676dd638cfd7475d9f3c607f5952968c9
Matt Davey <matt.davey@fsfe.org>
2019-12-29 01:45:14 +0000

    ChannelAgent now responds to a ConnectionStart command by spawning a child HandshakeAgent and awaiting the handshake result

--- Lapine.Core/Agents/ChannelAgent.cs
+++ Lapine.Core/Agents/ChannelAgent.cs
@@ -2,10 +2,16 @@ namespace Lapine.Agents {
     using System;
     using System.Threading.Tasks;
     using Lapine.Agents.Commands;
+    using Lapine.Agents.Events;
+    using Lapine.Agents.Middleware;
+    using Lapine.Protocol.Commands;
     using Proto;
+
+    using static Proto.Actor;
 
     public class ChannelAgent : IActor {
         readonly Behavior _behaviour;
+        PID _handshakeAgent;
 
         public ChannelAgent() =>
             _behaviour = new Behavior(Unstarted);
@@ -20,17 +26,75 @@ namespace Lapine.Agents {
                     break;
                 }
             }
-            return Actor.Done;
+            return Done;
         }
 
         Task Running(IContext context) {
             switch (context.Message) {
                 case TransmitCommand message: {
                     context.Send(context.Parent, message.Command);
-                    return Actor.Done;
+                    return Done;
+                }
+                case ConnectionStart message: {
+                    _handshakeAgent = context.SpawnNamed(
+                        Props.FromProducer(() => new HandshakeAgent("/"))
+                            .WithReceiveMiddleware(FramingMiddleware.UnwrapFrames(0))
+                            .WithContextDecorator(context => new LoggingContextDecorator(context)),
+                        "handshake"
+                    );
+                    context.Forward(_handshakeAgent);
+                    _behaviour.Become(AwaitHandshake);
+                    return Done;
+                }
+                case ConnectionClose message: {
+                    context.Send(context.Parent, new ConnectionCloseOk());
+                    context.Self.Stop();
+                    return Done;
                 }
             }
-            return Actor.Done;
+            return Done;
+        }
+
+        Task AwaitHandshake(IContext context) {
+            switch (context.Message) {
+                case ConnectionStartOk _: {
+                    context.Forward(context.Parent);
+                    return Done;
+                }
+                case ConnectionSecure _: {
+                    context.Forward(_handshakeAgent);
+                    return Done;
+                }
+                case ConnectionSecureOk _: {
+                    context.Forward(context.Parent);
+                    return Done;
+                }
+                case ConnectionTune _: {
+                    context.Forward(_handshakeAgent);
+                    return Done;
+                }
+                case ConnectionTuneOk _: {
+                    context.Forward(context.Parent);
+                    return Done;
+                }
+                case ConnectionOpen _: {
+                    context.Forward(context.Parent);
+                    return Done;
+                }
+                case ConnectionOpenOk _: {
+                    context.Forward(_handshakeAgent);
+                    return Done;
+                }
+                case HandshakeCompleted _: {
+                    _behaviour.Become(Running);
+                    return Done;
+                }
+                case AuthenticationFailed _: {
+                    context.Self.Stop(); // TODO: fail gracefully
+                    return Done;
+                }
+                default: return Done;
+            }
         }
     }
 }
