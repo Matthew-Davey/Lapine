59145ed50510ec452b936bb32e159b075426665b
Matt Davey <matt.davey@fsfe.org>
2019-12-31 11:58:16 +0000

    Add virtual host to connection configuration

--- Lapine.Core/ConnectionConfiguration.cs
+++ Lapine.Core/ConnectionConfiguration.cs
@@ -8,11 +8,15 @@ namespace Lapine {
         public IEnumerable<IPEndPoint> Endpoints { get; }
         public IEndpointSelectionStrategy EndpointSelectionStrategy { get; }
         public PeerProperties PeerProperties { get; }
+        public String VirtualHost { get; }
 
-        public ConnectionConfiguration(IEnumerable<IPEndPoint> endpoints, IEndpointSelectionStrategy endpointSelectionStrategy, PeerProperties peerProperties = null) {
+        public const String DefaultVirtualHost = "/";
+
+        public ConnectionConfiguration(IEnumerable<IPEndPoint> endpoints, IEndpointSelectionStrategy endpointSelectionStrategy, PeerProperties peerProperties = null, String virtualHost = null) {
             Endpoints                 = endpoints ?? throw new ArgumentNullException(nameof(endpoints));
             EndpointSelectionStrategy = endpointSelectionStrategy ?? throw new ArgumentNullException(nameof(endpointSelectionStrategy));
             PeerProperties            = peerProperties ?? PeerProperties.Default;
+            VirtualHost               = virtualHost ?? DefaultVirtualHost;
         }
 
         internal IEnumerator<IPEndPoint> GetEndpointEnumerator() =>
