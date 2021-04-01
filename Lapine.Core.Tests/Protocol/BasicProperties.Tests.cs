c75264ca857eaa465f20d8d644bee1ce74b7dba4
Matt Davey <matt.davey@fsfe.org>
2021-04-01 11:07:02 +0100

    Add unit tests for BasicProperties

---
+++ Lapine.Core.Tests/Protocol/BasicProperties.Tests.cs
@@ -0,0 +1,69 @@
+namespace Lapine.Protocol {
+    using System;
+    using System.Buffers;
+    using Bogus;
+    using Xunit;
+
+    public class BasicPropertiesTests : Faker {
+        [Fact]
+        public void SerializationIsSymmetric() {
+            var buffer = new ArrayBufferWriter<Byte>();
+            var value =  BasicProperties.Empty with {
+                AppId           = Random.Utf16String(),
+                ClusterId       = Random.Utf16String(),
+                ContentType     = Random.Utf16String(),
+                ContentEncoding = Random.Utf16String(),
+                CorrelationId   = Random.Utf16String(),
+                DeliveryMode    = Random.Byte(),
+                Expiration      = Random.Utf16String(),
+                MessageId       = Random.Utf16String(),
+                Priority        = Random.Byte(),
+                ReplyTo         = Random.Utf16String(),
+                Timestamp       = Random.ULong(),
+                Type            = Random.Utf16String(),
+                UserId          = Internet.UserName()
+            };
+
+            value.Serialize(buffer);
+            BasicProperties.Deserialize(buffer.WrittenMemory.Span, out var deserialized, out var _);
+
+            Assert.Equal(expected: value, actual: deserialized);
+        }
+
+        [Fact]
+        public void DeserializationFailsWithUnsufficientData() {
+            var result = BasicProperties.Deserialize(Array.Empty<Byte>(), out var _, out var _);
+
+            Assert.False(result);
+        }
+
+        [Fact]
+        public void DeserializationReturnsSurplusData() {
+            var value =  BasicProperties.Empty with {
+                AppId           = Random.Utf16String(),
+                ClusterId       = Random.Utf16String(),
+                ContentType     = Random.Utf16String(),
+                ContentEncoding = Random.Utf16String(),
+                CorrelationId   = Random.Utf16String(),
+                DeliveryMode    = Random.Byte(),
+                Expiration      = Random.Utf16String(),
+                MessageId       = Random.Utf16String(),
+                Priority        = Random.Byte(),
+                ReplyTo         = Random.Utf16String(),
+                Timestamp       = Random.ULong(),
+                Type            = Random.Utf16String(),
+                UserId          = Internet.UserName()
+            };
+            var extra = Random.UInt();
+            var buffer = new ArrayBufferWriter<Byte>();
+
+            buffer.WriteSerializable(value)
+                .WriteUInt32LE(extra);
+
+            BasicProperties.Deserialize(buffer.WrittenMemory.Span, out var _, out var surplus);
+
+            Assert.Equal(expected: sizeof(UInt32), actual: surplus.Length);
+            Assert.Equal(expected: extra, actual: BitConverter.ToUInt32(surplus));
+        }
+    }
+}
