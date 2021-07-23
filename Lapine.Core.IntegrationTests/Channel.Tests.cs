8548a55eb589fd900dea602e2849ff09f60c8aa5
Matt Davey <matt.davey@fsfe.org>
2021-07-23 23:17:49 +0100

    Add separate integration tests for declaring direct, fanout, headers and topic exchanges

--- Lapine.Core.IntegrationTests/Channel.Tests.cs
+++ Lapine.Core.IntegrationTests/Channel.Tests.cs
@@ -12,6 +12,75 @@ namespace Lapine {
         [Example("3.9-rc-alpine")]
         [Example("3.8-alpine")]
         [Example("3.7-alpine")]
+        public void DeclareDirectExchange(String brokerVersion, BrokerProxy broker, AmqpClient subject, Channel channel, ExchangeDefinition exchangeDefinition) {
+            $"Given a running RabbitMQ v{brokerVersion} broker".x(async () => {
+                broker = await BrokerProxy.StartAsync(brokerVersion);
+            }).Teardown(async () => await broker.DisposeAsync());
+            "And a client connected to the broker with an open channel".x(async () => {
+                subject = new AmqpClient(await broker.GetConnectionConfigurationAsync());
+                await subject.ConnectAsync();
+                channel = await subject.OpenChannelAsync();
+            }).Teardown(async () => await subject.DisposeAsync());
+            "When the client declares a topic exchange".x(async () => {
+                await channel.DeclareExchangeAsync(exchangeDefinition = ExchangeDefinition.Direct(Random.String2(12)));
+            });
+            "Then the exchange is created on the broker".x(async () => {
+                var exchanges = await broker.GetExchanges().ToListAsync();
+
+                exchanges.Should().Contain(exchangeDefinition);
+            });
+        }
+
+        [Scenario]
+        [Example("3.9-rc-alpine")]
+        [Example("3.8-alpine")]
+        [Example("3.7-alpine")]
+        public void DeclareFanoutExchange(String brokerVersion, BrokerProxy broker, AmqpClient subject, Channel channel, ExchangeDefinition exchangeDefinition) {
+            $"Given a running RabbitMQ v{brokerVersion} broker".x(async () => {
+                broker = await BrokerProxy.StartAsync(brokerVersion);
+            }).Teardown(async () => await broker.DisposeAsync());
+            "And a client connected to the broker with an open channel".x(async () => {
+                subject = new AmqpClient(await broker.GetConnectionConfigurationAsync());
+                await subject.ConnectAsync();
+                channel = await subject.OpenChannelAsync();
+            }).Teardown(async () => await subject.DisposeAsync());
+            "When the client declares a topic exchange".x(async () => {
+                await channel.DeclareExchangeAsync(exchangeDefinition = ExchangeDefinition.Fanout(Random.String2(12)));
+            });
+            "Then the exchange is created on the broker".x(async () => {
+                var exchanges = await broker.GetExchanges().ToListAsync();
+
+                exchanges.Should().Contain(exchangeDefinition);
+            });
+        }
+
+        [Scenario]
+        [Example("3.9-rc-alpine")]
+        [Example("3.8-alpine")]
+        [Example("3.7-alpine")]
+        public void DeclareHeadersExchange(String brokerVersion, BrokerProxy broker, AmqpClient subject, Channel channel, ExchangeDefinition exchangeDefinition) {
+            $"Given a running RabbitMQ v{brokerVersion} broker".x(async () => {
+                broker = await BrokerProxy.StartAsync(brokerVersion);
+            }).Teardown(async () => await broker.DisposeAsync());
+            "And a client connected to the broker with an open channel".x(async () => {
+                subject = new AmqpClient(await broker.GetConnectionConfigurationAsync());
+                await subject.ConnectAsync();
+                channel = await subject.OpenChannelAsync();
+            }).Teardown(async () => await subject.DisposeAsync());
+            "When the client declares a topic exchange".x(async () => {
+                await channel.DeclareExchangeAsync(exchangeDefinition = ExchangeDefinition.Headers(Random.String2(12)));
+            });
+            "Then the exchange is created on the broker".x(async () => {
+                var exchanges = await broker.GetExchanges().ToListAsync();
+
+                exchanges.Should().Contain(exchangeDefinition);
+            });
+        }
+
+        [Scenario]
+        [Example("3.9-rc-alpine")]
+        [Example("3.8-alpine")]
+        [Example("3.7-alpine")]
         public void DeclareTopicExchange(String brokerVersion, BrokerProxy broker, AmqpClient subject, Channel channel, ExchangeDefinition exchangeDefinition) {
             $"Given a running RabbitMQ v{brokerVersion} broker".x(async () => {
                 broker = await BrokerProxy.StartAsync(brokerVersion);
